448711334a92728627b74f28384717de014e1314
Kjames5269 <Kgrady1@email.arizona.edu>
2018-01-13 03:33:54 -0700

    No more node modules. we are talking to the database now bb

--- src/database.js
+++ src/database.js
@@ -1,17 +1,17 @@
-const secret = require('./secret.js')
+const { dbuser, pass } = require('./secret.js')
 const MongoClient = require('mongodb').MongoClient;
+const Promise = require('bluebird');
 
-const url=secret.db;
+const url="mongodb://" + dbuser + ":" + pass + "@ds253587.mlab.com:53587/hackaz2018";
 
 function base(queryFunc) {
   return new Promise((resolve, reject) => {
     MongoClient.connect(url, (err, db) => {
-      const col = db.collection('mangaList');
+      const col = db.collection('complements');
       const abst = queryFunc(col);
       abst((err,doc) => {
-        if(err === null) {
-          const retval = (doc != null) ? doc.mangaList : [];
-          resolve(retval);
+	if(err === null) {
+          resolve(doc);
         }
         else {
           reject(err);
@@ -22,74 +22,20 @@ function base(queryFunc) {
   });
 }
 
-export function getList(usr) {
-  return base((col) => col.findOne.bind(col, {"_id": usr}));
+export function getComplement() {
+  return base((col) => col.findOne.bind(col));
 }
 
 //export function get
 
-export function insertInto(usr, manga) {
-	console.log(manga);
-    return insert( usr, manga.name, manga.id, manga.currCh.ch,
-         manga.currCh.chId, manga.currCh.chName, manga.nextCh.ch,
-         manga.nextCh.chId, manga.nextCh.chName );
-}
-
-//  just a thousand parameters nothing to see here...
-function insert(usr, name, id, ch, chId, chName, nextCh, nextChId, nextChName) {
-  return base((col) => {
-    return col.findOneAndUpdate.bind(col,
-      { "_id": usr },
-      { "$pull": { "mangaList": { "id": id }}}
-    );
-  })
-  .then(() => {
+export function insertInto(usr, complement) {
     return base((col) => {
-      return col.findOneAndUpdate.bind(col,
-        { "_id": usr },
-        { "$push": { mangaList: {
-          "name": name,
-          "id": id,
-          "currCh": {
-              "ch": ch, "chId": chId,
-              "chName": chName
-          },
-          "nextCh": {
-              "ch": nextCh, "chId": nextChId,
-              "chName": nextChName
-          }
-        }}},
-        { "upsert": true }
-      );
-    });
-  });
-}
-
-export function removeManga(usr, mangaName) {
-    return base((col) => {
-        return col.findOneAndUpdate.bind(col,
-        { "_id": usr },
-        { "$pull": { "mangaList": { "name": mangaName }}});
-    });
-}
-
-export function setChapter(usr, manga) {
-    return setCh( usr, manga.id, manga.currCh.ch, manga.currCh.chId,
-        manga.currCh.chName, manga.nextCh.ch, manga.nextCh.chId,
-        manga.nextCh.chName
-     );
-}
-
-function setCh(usr, id, ch, chId, chName, nextCh, nextChId, nextChName) {
-  return base((col) => {
-    return col.findOneAndUpdate.bind(col,
-      { "_id": usr, "mangaList.id": { $eq: id }},
-      { "$set": { "mangaList.$.currCh.ch": ch,
-                  "mangaList.$.currCh.chId": chId,
-                  "mangaList.$.currCh.chName": chName,
-                  "mangaList.$.nextCh.ch": nextCh,
-                  "mangaList.$.nextCh.chId": nextChId,
-                  "mangaList.$.nextCh.chName": nextChName }}
-    )
-  })
+	return col.findOneAndUpadate.bind(col,
+	    { 
+	      "quote": complement,
+	      "user": usr 
+	    },
+	    { "upsert": true }
+	);
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const Promise = require('bluebird');
 const express = require('express');
+const DB = require('./database.js');
 
 var app = express();
 
 app.get('/', (req, res) => {
-	res.send("Hello World!");
+	DB.getComplement().then((doc) => {
+		res.send(doc);
+	})
 });
 
-app.listen(8080, () => {
+app.post('/', (req, res) => {
+	res.send("Meee too thank");
+});
+
+app.listen(8081, () => {
   console.log("Listening now on port 8080!");
 });
