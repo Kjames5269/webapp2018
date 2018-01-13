1f01bd6dfca78c31bc3dd9009bec7da8317fcf5e
Kjames5269 <Kgrady1@email.arizona.edu>
2018-01-13 02:09:31 -0700

    Hello world!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Heyy");
+const Promise = require('bluebird');
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+  console.log("Listening now on port 8080!");
+});
